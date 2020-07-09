6a8658d4d927a0bc98d89ef3b92702675d34e4f5
wangchangshui <wangchangshui@baidu.com>
2020-07-09 15:58:10 +0800

    fix sts

--- services/sts/client.go
+++ services/sts/client.go
@@ -40,22 +40,25 @@ func (c *Client) GetSessionToken(duration int, acl string) (*api.GetSessionToken
 
 // NewClient make the STS service client with default configuration.
 // Use `cli.Config.xxx` to access the config or change it to non-default value.
-func NewClient(ak, sk string) (*Client, error) {
+func NewClient(ak, sk, endpoint string) (*Client, error) {
 	credentials, err := auth.NewBceCredentials(ak, sk)
 	if err != nil {
 		return nil, err
+	}
+	if len(endpoint) == 0 {
+		endpoint = bce.DEFAULT_REGION
 	}
 	defaultSignOptions := &auth.SignOptions{
 		auth.DEFAULT_HEADERS_TO_SIGN,
 		util.NowUTCSeconds(),
 		auth.DEFAULT_EXPIRE_SECONDS}
 	defaultConf := &bce.BceClientConfiguration{
-		Endpoint:                  DEFAULT_SERVICE_DOMAIN,
-		Region:                    bce.DEFAULT_REGION,
-		UserAgent:                 bce.DEFAULT_USER_AGENT,
-		Credentials:               credentials,
-		SignOption:                defaultSignOptions,
-		Retry:                     bce.DEFAULT_RETRY_POLICY,
+		Endpoint:    DEFAULT_SERVICE_DOMAIN,
+		Region:      endpoint,
+		UserAgent:   bce.DEFAULT_USER_AGENT,
+		Credentials: credentials,
+		SignOption:  defaultSignOptions,
+		Retry:       bce.DEFAULT_RETRY_POLICY,
 		ConnectionTimeoutInMillis: bce.DEFAULT_CONNECTION_TIMEOUT_IN_MILLIS}
 	v1Signer := &auth.BceV1Signer{}
 
